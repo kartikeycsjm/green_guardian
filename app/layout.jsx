198900c7b1be551d963904482a5773925d11b834
Kartikey Mishra <kartikeymishracsjm@gmail.com>
2025-05-16 12:20:11 +0530

    remove name

--- app/layout.jsx
+++ app/layout.jsx
@@ -29,7 +29,6 @@ export default function RootLayout({ children }) {
               <nav className="flex justify-between items-center">
                 <Link href="/" className="text-xl font-bold">Green Guardian</Link>
                 <ul className="flex space-x-4 text-sm">
-                  <li><Link href="/plant-name" className="hover:text-green-200">Plant Name</Link></li>
                   <li><Link href="/plant-disease" className="hover:text-green-200">Plant Disease</Link></li>
                 </ul>
               </nav>
